4531931d370dd90276be8d752cf91bdb3438f23a
Andy <admazzola@gmail.com>
2015-03-10 22:36:11 -0400

    Changed version number

--- src/launcher/ui/WebsiteTab.java
+++ src/launcher/ui/WebsiteTab.java
@@ -1,7 +1,9 @@
 package launcher.ui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.io.IOException;
 import java.net.URL;
 
 import javax.swing.JEditorPane;
@@ -14,13 +16,25 @@ import javax.swing.event.HyperlinkEvent.EventType;
 import launcher.Launcher;
 import launcher.OperatingSystem;
 
+import org.fit.cssbox.layout.BrowserCanvas;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.fit.cssbox.css.DOMAnalyzer;
+import org.fit.cssbox.*;
+import org.fit.cssbox.swingbox.*;
+import org.fit.cssbox.swingbox.demo.SwingBrowserHyperlinkHandler;
+import org.fit.cssbox.swingbox.util.GeneralEventListener;
+import org.fit.net.DataURLHandler;
+
 
 public class WebsiteTab extends JScrollPane{
 
-	JEditorPane blog = new JEditorPane();
 	
+    
 	public WebsiteTab(){
 		
+		 
+		/*
 		blog.setEditable(false);
 		blog.setMargin(null);
 		blog.setBackground(Color.DARK_GRAY);
@@ -43,20 +57,62 @@ public class WebsiteTab extends JScrollPane{
 			
 		});
 		
+		*/
 		
-		setViewportView(blog);
 		
 	}
+	
+	public void displayURL(String urlstring)
+    {
+        try {
+            if (!urlstring.startsWith("http:") &&
+                    !urlstring.startsWith("https:") &&
+                    !urlstring.startsWith("ftp:") &&
+                    !urlstring.startsWith("file:") &&
+                    !urlstring.startsWith("data:"))
+                        urlstring = "http://" + urlstring;
+                
+            URL url = DataURLHandler.createURL(null, urlstring);
+           
+
+            displayURLSwingBox(url);
+        } catch (Exception e) {
+            System.err.println("*** Error: "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+	 BrowserPane swingbox = null;
+	 
+    protected void displayURLSwingBox(URL url) throws IOException
+    {
+        if (swingbox == null)
+        {
+            swingbox = new BrowserPane();
+           this.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_NEVER);
+           this.setHorizontalScrollBarPolicy(HORIZONTAL_SCROLLBAR_NEVER);
+            swingbox.setPreferredSize(new Dimension(300, 200));
+            //this.add( swingbox );
+            setViewportView(swingbox);
+        }
+        swingbox.setPage(url);
+    }
+    
+    
+    
+ 
 	
 	public void setPage(final String url) {
 			    Thread thread = new Thread("Update website tab")
 			    {
 			      public void run() {
+			    	  displayURL( url );
+			    	 
 			        try {
-			          blog.setPage(new URL(url));
+			        //  blog.setPage(new URL(url));
 			        } catch (Exception e) {
 			          Launcher.println("Unexpected exception loading " + url);
-			          blog.setText("<html><body><font color=\"#808080\"><br><br><br><br><br><br><br><center><h1>Failed to get page</h1><br>" + e.toString() + "</center></font></body></html>");
+			        //  blog.setText("<html><body><font color=\"#808080\"><br><br><br><br><br><br><br><center><h1>Failed to get page</h1><br>" + e.toString() + "</center></font></body></html>");
 			        }
 			      }
 			    };
